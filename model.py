2225cd572661517a547dec82ce2191dad23681fd
MusouCrow <939549924@qq.com>
2019-08-19 00:42:07 +0800

    Completed.

--- model.py
+++ model.py
@@ -3,24 +3,29 @@ from torch import nn
 
 
 class Model(Module):
-	def __init__(self):
-		super(Model, self).__init__()
-		self.conv1 = nn.Conv2d(1, 6, 5)
-		self.pool1 = nn.MaxPool2d(2)
-		self.conv2 = nn.Conv2d(6, 16, 5)
-		self.pool2 = nn.MaxPool2d(2)
-		self.fc1 = nn.Linear(400, 120)
-		self.fc2 = nn.Linear(120, 84)
-		self.fc3 = nn.Linear(84, 10)
+    def __init__(self):
+        super(Model, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.pool1 = nn.MaxPool2d(2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.pool2 = nn.MaxPool2d(2)
+        self.fc1 = nn.Linear(256, 120)
+        self.tanh1 = nn.ReLU()
+        self.fc2 = nn.Linear(120, 84)
+        self.tanh2 = nn.ReLU()
+        self.fc3 = nn.Linear(84, 10)
+        self.tanh3 = nn.ReLU()
 
-	def forward(self, x):
-		y = self.conv1(x)
-		y = self.pool1(y)
-		y = self.conv2(y)
-		y = self.pool2(y)
-		y = y.view(y.shape[0], -1)
-		y = self.fc1(y)
-		y = self.fc2(y)
-		y = self.fc3(y)
-
-		return y
+    def forward(self, x):
+        y = self.conv1(x)
+        y = self.pool1(y)
+        y = self.conv2(y)
+        y = self.pool2(y)
+        y = y.view(y.shape[0], -1)
+        y = self.fc1(y)
+        y = self.tanh1(y)
+        y = self.fc2(y)
+        y = self.tanh2(y)
+        y = self.fc3(y)
+        y = self.tanh3(y)
+        return y
--- train.py
+++ train.py
@@ -2,13 +2,43 @@ from model import Model
 import numpy as np
 import torch
 from torchvision.datasets import mnist
-
+from torch.nn import CrossEntropyLoss
+from torch.optim import SGD
+from torch.utils.data import DataLoader
+from torchvision.transforms import ToTensor
 
 if __name__ == '__main__':
+    batch_size = 256
+    train_dataset = mnist.MNIST(root='./train', train=True, transform=ToTensor())
+    test_dataset = mnist.MNIST(root='./test', train=False, transform=ToTensor())
+    train_loader = DataLoader(train_dataset, batch_size=batch_size)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size)
+    model = Model()
+    sgd = SGD(model.parameters(), lr=1e-1)
+    cross_error = CrossEntropyLoss()
+    epoch = 100
 
-	train_data = mnist.MNIST(root='./train', train=True, download=True)
-	test_data = mnist.MNIST(root='./test', train=False, download=True)
+    for _epoch in range(epoch):
+        for idx, (train_x, train_label) in enumerate(train_loader):
+            label_np = np.zeros((train_label.shape[0], 10))
+            sgd.zero_grad()
+            predict_y = model(train_x.float())
+            _error = cross_error(predict_y, train_label.long())
+            if idx % 10 == 0:
+                print('idx: {}, _error: {}'.format(idx, _error))
+            _error.backward()
+            sgd.step()
 
-	random_X = np.random.randn(1, 1, 32, 32)
-	model = Model()
-	random_y = model.forward(torch.FloatTensor(random_X))
+        correct = 0
+        _sum = 0
+
+        for idx, (test_x, test_label) in enumerate(test_loader):
+            predict_y = model(test_x.float()).detach()
+            predicy_ys = np.argmax(predict_y, axis=-1)
+            label_np = test_label.numpy()
+            _ = predicy_ys == test_label
+            correct += np.sum(_.numpy(), axis=-1)
+            _sum += _.shape[0]
+
+        print('accuracy: {:.2f}'.format(correct / _sum))
+        torch.save(model, 'models/mnist_{:.2f}.pkl'.format(correct / _sum))
